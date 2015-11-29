9880d6977ed67ebe77bfbcf7d16fb4bc26674880
James McGeachie <jamesmcgeachie10@gmail.com>
2015-11-28 23:22:27 -0500

    Completed some simple refactoring and added comments to the js file

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 $(document).ready(function() {
 
+	// Sets up board as a collection of all divs with the class of box and setting turn # to 0.
 	var board = $('div .box');
 	var turn = 0;
 
+	// Checks if a string of 3 box values will equal a string of 3 of the symbol within the box.
 	function win(box1, box2, box3, symbol) {
-		if ( $(box1).text() + $(box2).text() + $(box3).text() === symbol + symbol + symbol ) {
-			$('#winbox').html( symbol + ' Player Wins!' );
-			$('#gamestage').html('Game Over.');
+		if ( box1.text() + box2.text() + box3.text() === symbol + symbol + symbol ) {
+			$('#gamestage').html( symbol + ' Player Wins!' ).css({'color': 'orange', 'fontWeight': 'bold'});
 			board.off('click');
 			if ( symbol === 'X' ) {
-				$(box1).addClass('winsquaresx');
-				$(box2).addClass('winsquaresx');
-				$(box3).addClass('winsquaresx');
+				box1.addClass('winsquaresx');
+				box2.addClass('winsquaresx');
+				box3.addClass('winsquaresx');
 			} else {
-				$(box1).addClass('winsquareso');
-				$(box2).addClass('winsquareso');
-				$(box3).addClass('winsquareso');
+				box1.addClass('winsquareso');
+				box2.addClass('winsquareso');
+				box3.addClass('winsquareso');
 			};
 		};
 	};
 
+	// Takes a given id and returns a JQuery object for the element with that id
 	function box(id) {
-		return '#' + id;
+		return $('#' + id);
 	};
 
-
+	// Wraps the actions that will be taken in each turn
 	function processTurn() {
 
+		// Populates clicked element with an X or O dependent on turn #,
+		// increments the turn and turns off click event
 		function addSymbol(square) {
 
 			if (turn % 2 == 0) {
@@ -38,6 +42,7 @@ $(document).ready(function() {
 		  $(square).off('click');
 		};
 
+		// Uses the win function to check every winning combination
 		function checkWin() {
 			win(box('1'), box('2'), box('3'), 'X');
 			win(box('4'), box('5'), box('6'), 'X');
@@ -57,22 +62,27 @@ $(document).ready(function() {
 			win(box('3'), box('5'), box('7'), 'O');
 		};
 
+		// Displays whose turn it is, up until turn 9 at which point
+		// if the game hasn't ended it will display tied game.
 		function whoseTurn() {
 			if (turn % 2 == 0 && turn < 9 ) {
 				$('#gamestage').html('X player to move');
 			} else if ( !(turn % 2 == 0) && turn < 9 ) {
 				$('#gamestage').html('O player to move');
 			} else {
-				$('#gamestage').html('Tied Game');
+				$('#gamestage').html('Tied Game').css({'color': 'grey', 'fontWeight': 'bold'});
 			}
 		};
 
+		// Calls the 3 functions defined in the processTurn function
 		addSymbol(this);
 		whoseTurn();
 		checkWin();
 
 	};
 
+	// Listens for a click on any element stored in the board
+	// variable and runs processTurn when event fired
 	board.on('click', processTurn);
 
 });
